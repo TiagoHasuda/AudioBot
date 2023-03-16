69407107a3c5b0bcc73cf298cf63cc279697f5b5
Tiago Grossi Hasuda <tiago.ghasuda@gmail.com>
2023-03-16 15:02:08 -0300

    logs: added logs

--- discord/interactions/reset.ts
+++ discord/interactions/reset.ts
@@ -5,9 +5,10 @@ import { AudioPlayerSingleton } from "../audioplayer";
 export async function reset(_: string, interaction: Interaction<CacheType>) {
   const player = AudioPlayerSingleton.getAudioPlayer()
   await player.disconnect(interaction.client)
+  console.log({ interaction, channel: interaction.channel, guild: interaction.guild })
   const connection = joinVoiceChannel({
-    channelId: interaction.channelId as string,
-    guildId: interaction.guildId as string,
+    channelId: interaction.channel?.id as string,
+    guildId: interaction.guild?.id as string,
     adapterCreator: interaction.guild?.voiceAdapterCreator as InternalDiscordGatewayAdapterCreator,
   })
   player.subscribe(connection)
